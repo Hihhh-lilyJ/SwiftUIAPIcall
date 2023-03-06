d8caff59957176ac597a82372c9762bc703e0f4c
Hihhh-lilyJ <eggylilwin@gmail.com>
2023-03-06 16:03:06 +0630

    Initial Commit

---
+++ SwiftUIAPIcall/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  SwiftUIAPIcall
+//
+//  Created by MacBook Pro on 06/03/2023.
+//
+
+import SwiftUI
+
+struct URLImage : View {
+    let urlString : String
+    
+    @State var data: Data?
+    var body: some View{
+        if
+            let data = data,
+            let uiimage = UIImage(data: data) {
+                Image(uiImage: uiimage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 130, height: 70)
+                    .background(Color.gray)
+            }
+        else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+                .onAppear {
+                    fetchData()
+            }
+        }
+    }
+    private func fetchData() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            self.data = data
+        }
+        task.resume()
+    }
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(viewModel.courses, id:\.self) {
+                    course in
+                    HStack{
+                        URLImage(urlString: course.image)
+                        Text(course.name)
+                            .bold()
+                    }
+                    .padding(3)
+                }
+            }
+            .navigationTitle("Courses")
+            .onAppear {
+                viewModel.fetch()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAPIcall/SwiftUIAPIcallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPIcallApp.swift
+//  SwiftUIAPIcall
+//
+//  Created by MacBook Pro on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPIcallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
